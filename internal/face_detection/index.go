1ad18762454bb348a1ddca4ccb156508742bcb10
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-01-03 19:55:55 +0300

    init code

---
+++ internal/face_detection/index.go
@@ -0,0 +1,47 @@
+package face_detection
+
+import (
+	"context"
+	"log"
+)
+
+// Структура запроса API Gateway v1
+type APIGatewayRequest struct {
+	OperationID string `json:"operationId"`
+	Resource    string `json:"resource"`
+
+	HTTPMethod string `json:"httpMethod"`
+
+	Path           string            `json:"path"`
+	PathParameters map[string]string `json:"pathParameters"`
+
+	Headers           map[string]string   `json:"headers"`
+	MultiValueHeaders map[string][]string `json:"multiValueHeaders"`
+
+	QueryStringParameters           map[string]string   `json:"queryStringParameters"`
+	MultiValueQueryStringParameters map[string][]string `json:"multiValueQueryStringParameters"`
+
+	Parameters           map[string]string   `json:"parameters"`
+	MultiValueParameters map[string][]string `json:"multiValueParameters"`
+
+	Body            string `json:"body"`
+	IsBase64Encoded bool   `json:"isBase64Encoded,omitempty"`
+
+	RequestContext interface{} `json:"requestContext"`
+}
+
+// Структура ответа API Gateway v1
+type APIGatewayResponse struct {
+	StatusCode        int                 `json:"statusCode"`
+	Headers           map[string]string   `json:"headers"`
+	MultiValueHeaders map[string][]string `json:"multiValueHeaders"`
+	Body              string              `json:"body"`
+	IsBase64Encoded   bool                `json:"isBase64Encoded,omitempty"`
+}
+
+func Handler(ctx context.Context, event *APIGatewayRequest) (*APIGatewayResponse, error) {
+	log.Println(event)
+	return &APIGatewayResponse{
+		StatusCode: 200,
+	}, nil
+}
---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+locals {
+  home = "/home/www"
+}
+
+variable "cloud_id" {
+  type = string
+  description = "Идентификатор облака"
+}
+
+variable "folder_id" {
+  type = string
+  description = "Идентификатор каталога"
+}
+
+provider "yandex" {
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+  service_account_key_file = "${local.home}/.yc-keys/key.json"
+}
+
+resource "yandex_iam_service_account" "func-bot-account" {
+  name        = "func-bot-account"
+  description = "Аккаунт для функции"
+  folder_id   = var.folder_id
+}
+
+resource "archive_file" "zip" {
+  type = "zip"
+  output_path = "src.zip"
+  source_dir = "internal"
+}
+
+resource "yandex_storage_bucket" "input-bucket" {
+  bucket = "vvot14-photo"
+  folder_id = var.folder_id
+}
+
+resource "yandex_function" "face_detect" {
+  name        = "vvot14-face-detection"
+  user_hash   = archive_file.zip.output_sha256
+  runtime     = "golang121"
+  entrypoint  = "index.Handler"
+  memory      = 128
+  execution_timeout  = 10
+
+  service_account_id = yandex_iam_service_account.func-bot-account.id
+
+  storage_mounts {
+    mount_point_name = "images"
+    bucket = yandex_storage_bucket.input-bucket.id
+    prefix           = ""
+  }
+
+  content {
+    zip_filename = archive_file.zip.output_path
+  }
+}
+
+resource "yandex_function_trigger" "input_trigger" {
+  name        = "vvot14-photo"
+  description = "Триггер для запуска обработчика vvot14-face-detection"
+  function {
+    id                 = yandex_function.face_detect.id
+    service_account_id = yandex_iam_service_account.func-bot-account.id
+    retry_attempts     = 2
+  }
+  object_storage {
+    bucket_id    = yandex_storage_bucket.input-bucket.id
+    suffix       = ".jpg"
+    create       = true
+    update       = false
+    delete       = false
+    batch_cutoff = 2
+  }
+}
