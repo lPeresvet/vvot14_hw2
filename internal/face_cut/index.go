b5259c4727281236a796752627fc401a7ec6ae45
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-01-05 12:21:57 +0300

    add cutter draft

--- internal/face_cut/index.go
+++ internal/face_cut/index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 )
 
@@ -10,8 +11,24 @@ type Response struct {
 	Body       interface{} `json:"body"`
 }
 
+type Messages struct {
+	Messages []struct {
+		Details struct {
+			Message struct {
+				Body string `json:"body"`
+			} `json:"message"`
+		} `json:"details"`
+	} `json:"messages"`
+}
+
 func Handler(ctx context.Context, request []byte) (*Response, error) {
-	log.Println(string(request))
+	messages := &Messages{}
+
+	if err := json.Unmarshal(request, messages); err != nil {
+		return nil, err
+	}
+
+	log.Println(messages)
 
 	return &Response{
 		StatusCode: 200,
