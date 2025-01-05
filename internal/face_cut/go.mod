4bca1e708e4d1a22162f24a8c1fd0a4030f428de
Sluchaev Kirill <k.sluchaev@yandex.ru>
2025-01-05 12:52:06 +0300

    add cutter draft

---
+++ internal/face_cut/go.mod
@@ -0,0 +1,10 @@
+module 2hw
+
+go 1.21.0
+
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/google/uuid v1.6.0
+)
+
+require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
--- internal/face_cut/index.go
+++ internal/face_cut/index.go
@@ -3,7 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"image"
 	"log"
+	"path"
+
+	"github.com/disintegration/imaging"
+	"github.com/google/uuid"
 )
 
 type Response struct {
@@ -15,17 +20,49 @@ type Messages struct {
 	Messages []struct {
 		Details struct {
 			Message struct {
-				Body string `json:"body"`
+				Body CutterTask `json:"body"`
 			} `json:"message"`
 		} `json:"details"`
 	} `json:"messages"`
 }
+
+type CutterTask struct {
+	Bounds   FaceBounds `json:"bounds"`
+	ObjectID string     `json:"objectID"`
+}
+
+type FaceBounds struct {
+	X      int `json:"x"`
+	Y      int `json:"y"`
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+const inputDir = "images"
 
 func Handler(ctx context.Context, request []byte) (*Response, error) {
 	messages := &Messages{}
 
 	if err := json.Unmarshal(request, messages); err != nil {
 		return nil, err
+	}
+
+	for _, msg := range messages.Messages {
+		img, err := imaging.Open(path.Join(inputDir, msg.Details.Message.Body.ObjectID))
+		if err != nil {
+			return nil, err
+		}
+
+		bounds := msg.Details.Message.Body.Bounds
+
+		rectcropimg := imaging.Crop(img, image.Rect(
+			bounds.X, bounds.Y,
+			bounds.X+bounds.Width,
+			bounds.Y+bounds.Height))
+
+		if err := imaging.Save(rectcropimg, path.Join(inputDir, uuid.New().String())); err != nil {
+			return nil, err
+		}
 	}
 
 	log.Println(messages)
